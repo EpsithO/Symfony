977b2aa488b2719c9282b39eff06278c5b9a8260
Maire Mathéo <mairem@SIOPERGAUD.FR>
2023-09-12 15:49:03 +0200

    DevWip1

---
+++ src/Controller/BienvenueController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BienvenueController extends AbstractController
+{
+    #[Route('/bienvenue', name: 'app_bienvenue')]
+    public function bienvenue(): Response
+    {
+        // Appelle simulé au modèle
+        // Apelle a la vue
+        return $this->render('bienvenue/index.html.twig',[
+        ]);
+
+    }
+
+    #[Route('/bienvenue/{prenom}', name: 'app_Gotham')]
+    public function bienvenuePrenom(string $prenom): Response
+    {
+        // Appelle simulé au modèle
+            $prenom = 'Mathéo';
+        // Apelle a la vue
+        return $this->render('bienvenue/index.html.twig',[
+            'prenom' => $prenom,
+        ]);
+
+    }
+
+    #[Route('/bienvenues', name: 'app_bienvenues')]
+    public function bienvenues(): Response
+    {
+        // Appelle simulé au modèle
+        $EquipageOnePiece = ["Luffy", "Zoro", "Nami", "Usopp", "Sanji", "Chopper", "Robin", "Franky", "Brook", "Jinbe"];
+        // Apelle a la vue
+        return $this->render('bienvenue/bienvenues.html.twig',[
+            'EquipageOnePiece' => $EquipageOnePiece,
+        ]);
+
+    }
+
+}
